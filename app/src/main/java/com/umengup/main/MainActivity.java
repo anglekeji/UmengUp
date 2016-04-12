00dac9efc5fb6e0b02b8998bcece28aed235f95c
xinying <2850255391@qq.com>
2016-04-12 15:43:35 +0800

    ceshixiugai

--- app/src/main/java/com/umengup/main/MainActivity.java
+++ app/src/main/java/com/umengup/main/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,7 +24,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         
         //UmengUpdateAgent.setUpdateOnlyWifi(false);//无论什么网络环境下都提醒，默认只在wifi下提醒
-        UmengUpdateAgent.update(this);     
+        UmengUpdateAgent.update(this);
+        Log.i("nihao","aaaa");
     }
 
 }
